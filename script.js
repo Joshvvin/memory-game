c2fb5f9dc8988626685b2b88ee13fbf1f61fc054
Joshvvin Joshy <joshvvinjoshy@gmail.com>
2023-08-25 15:11:06 +0530

    Made the memory game project responsive to mobile devices. - Made the bestscore variable to store the best score of all the matches played. - Made the score variable to show the user the number of guesses made.

--- script.js
+++ script.js
@@ -70,17 +70,22 @@ function createDivsForColors(colorArray) {
 let count = 0;
 let prevCard = 'null';
 let currentCard = 'null';
+let score = 0;
+localStorage.setItem('score', 100);
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
   // console.log(event.target)
   currentCard = event.target;
   // console.log('inside handlecardclick function of click event');
   // console.log(prevCard, currentCard);
+  const score_var = document.querySelector('.score_var');
+  // score_var.textContent = score;
   if(currentCard.style.backgroundColor == 'white'){
     // console.log('inside if block of click event');
     // currentCard.style.backgroundColor = 'red';
     // currentCard.style.backgroundColor = 'blue';
     currentCard.style.backgroundColor = currentCard.getAttribute('class'); 
+    currentCard.removeEventListener('click', handleCardClick);
     // currentCard.classList.append('rotate');
     // const currId = currentCard.getAttribute('id');
     // console.log('event')  
@@ -88,23 +93,64 @@ function handleCardClick(event) {
     // console.log(count);
     // if((prevCard == 'null') || (prevCard != 'null' && prevCard != currentCard)){
       count++;
-    // }
+      // }
+      // console.log(localStorage.getItem('score'));
+      
+      // const sc = localStorage.getItem('score');
+      // console.log(sc);
       if(count == 2){
+        score++;
+        score_var.textContent = score;
         count = 0;
         // console.log('inside if', count, prevCard, currentCard);
         // console.log(prevCard, currentCard);
+        
         // const prevId = prevCard.getAttribute('id');
         const prevColor = prevCard.getAttribute('class');
         const currColor = currentCard.getAttribute('class');      
         // console.log(currId, prevId);
         if( currColor == prevColor ){
           prevCard.removeEventListener('click', handleCardClick);
-          currentCard.removeEventListener('click', handleCardClick);
+          let c = 0;
+          for(const card of gameContainer.children){
+            if(card.getAttribute('class') == card.style.backgroundColor){
+              c++;
+            }
+          }
+          if(c == gameContainer.children.length){
+            const score_cont = document.querySelector('.score_var');
+            const bestscore_cont = document.querySelector('.bestscore');
+            // console.log(score.textContent, bestscore.textContent, localStorage.getItem('score'));
+            const score = parseInt(score_cont.textContent);
+            let bestscore = parseInt(bestscore_cont.textContent);
+            const locstore = parseInt(localStorage.getItem('score'));
+            console.log(score, bestscore, locstore);
+            if(score < locstore){
+              // console.log('inside score < locstore');
+              bestscore = score;
+              bestscore_cont.textContent = bestscore;
+              localStorage.setItem('score', score);
+            }
+            // console.log('outside if');
+            const restart = document.querySelector('.restart');
+            restart.style.display = 'flex';   
+            // console.log(score, bestscore.textContent, localStorage.getItem('score'));
+          }
         }
-        else{
+        else{         
+          for(const card of gameContainer.children){
+            if(card.getAttribute('id') != currentCard.getAttribute('id') && card.getAttribute('id') != prevCard.getAttribute('id')){
+              card.removeEventListener('click', handleCardClick);
+            }
+          }
           setTimeout(()=>{
             currentCard.style.backgroundColor = 'white';
             prevCard.style.backgroundColor = 'white';
+            for(const card of gameContainer.children){
+              // if(card.getAttribute('id') != currentCard.getAttribute('id') && card.getAttribute('id') != prevCard.getAttribute('id')){
+                card.addEventListener('click', handleCardClick);
+              // }
+            }
           },1000);          
         }
       }
@@ -130,3 +176,30 @@ function handleCardClick(event) {
 
 // // when the DOM loads
 createDivsForColors(shuffledColors);
+
+const start = document.querySelector('.start');
+function startgame(){
+  const display_button = document.querySelector('.display_button');
+  display_button.style.display = 'none';
+  gameContainer.style.display = 'flex';
+  const score_container = document.querySelector('.score_container');
+  score_container.style.display = 'flex';
+  const container = document.querySelector('.container');
+  // container.style.justifyContent = 'space-between';
+}
+start.addEventListener('click', startgame);
+
+const restart_game = document.querySelector('.restart_game')
+const restart = document.querySelector('.restart');
+function restartgame(){
+  restart.style.display = 'none';
+  gameContainer.style.display = 'flex';
+  score = 0;
+  const score_cont = document.querySelector('.score_var');
+  score_cont.textContent = score;
+  for(const card of gameContainer.children){
+    card.style.backgroundColor = 'white';
+    card.addEventListener('click', handleCardClick);
+  }
+}
+restart_game.addEventListener('click', restartgame);
